f95b1540f262af54d59403ba3871149b0b1f6d71
ymoturi <77892896+yashwanthmoturi@users.noreply.github.com>
2024-01-12 07:08:22 -0800

    added Jan11 assignment

---
+++ ProgramJan11Assignment.java
@@ -0,0 +1,10 @@
+public class ProgramJan11Assignment {
+    public static void main(String []args) {
+        int a = 40;
+        int b = 20;
+        System.out.println("a + b = "+(a+b));
+        System.out.println("a - b = "+(a-b));
+        System.out.println("a * b = "+(a*b));
+        System.out.println("a / b = "+(a/b));
+    }
+}
