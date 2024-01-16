f96937d516395ced3eb2ca48ae825fb7384e060f
ymoturi <77892896+yashwanthmoturi@users.noreply.github.com>
2024-01-15 17:55:11 -0800

    updating Jan 15 assignment

---
+++ ProgramJan15Assignment.java
@@ -0,0 +1,29 @@
+public class ProgramJan15Assignment {
+    public static void main(String [] args) {
+        String name = "Yash";
+        int age = 22;
+        boolean MS_Student = true;
+        System.out.println("Bio Data:");
+        System.out.println("Name: "+ name);
+        System.out.println("age: "+age);
+        System.out.println("Masters of Science Student: " + MS_Student);
+        String[] names = {
+                "Alice Smith",
+                "Bob Johnson",
+                "Charlie Brown",
+                "David Miller",
+                "Eva Davis",
+                "Frank Wilson",
+                "Grace Lee",
+                "Harry Taylor",
+                "Ivy Turner",
+                "Jack Harris"
+        };
+        System.out.println("3rd name from the array is : " + names[2]);
+        System.out.println("-5 + 8 * 6 = " + (-5 + 8 * 6));
+        System.out.println("(55+9) % 9 " + ((55+9) % 9));
+        System.out.println("20 + -3*5 / 8 = " + (20 + -3*5 / 8));
+        System.out.println("5 + 15 / 3 * 2 - 8 % 3 = " + (5 + 15 / 3 * 2 - 8 % 3));
+
+    }
+}
